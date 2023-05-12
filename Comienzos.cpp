f4519693e727360e323ee5d37c96deccaf12c367
FabioB8a <fabiobuitra@gmail.com>
2023-05-12 17:42:19 -0500

    first commit

---
+++ Comienzos.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout<<"Buenas buenas"<<endl;
+    return 0;
+}
---
+++ Manager.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char **argv){
+
+    char *nomPipe = NULL;
+    int cantidadTalkers = 0;
+
+    if (argc != 5)
+    {
+        printf(" -> Número inválido de argumentos.\n");
+        printf(" -> Recuerda: Uso correcto del ejecutable: ./Manager -n cantidadTalkers -p nombrePipe\n");
+        exit(1);
+    }
+
+    for (int i = 1; i < argc; i += 2)
+    {
+        if (strcmp(argv[i], "-n") == 0 && cantidadTalkers == 0)
+        {
+            cantidadTalkers = atoi(argv[i + 1]);
+        }
+        else if (strcmp(argv[i], "-p") == 0 && !nomPipe)
+        {
+            nomPipe = argv[i + 1];
+        }
+        else
+        {
+            printf(" -> Argumento inválido o repetido: %s.\n", argv[i]);
+        }
+    }
+
+    if (cantidadTalkers <= 0)
+    {
+        printf(" -> La cantidad de Talkers debe ser mayor a 0\n");
+    }
+}
